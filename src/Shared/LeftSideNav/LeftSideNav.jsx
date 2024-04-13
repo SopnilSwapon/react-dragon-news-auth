714362d9952427ac5527c32dd0020833fc21fcbb
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-04-13 22:56:24 +0600

    added left side of categories information

--- src/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
     return (
         <div>
-            <h2>Left side nav</h2>
+            <h2 className="text-3xl font-bold">Categories</h2>
+
+            <div className="mt-5">
+            {
+                categories.map(category => <NavLink 
+                    to={`category${category.id}`}
+                    key={category.id}
+                    className='block border font-semibold text-xl p-2'
+                    >{category.name}
+                    </NavLink>)
+            }
+            </div>
         </div>
     );
 };
